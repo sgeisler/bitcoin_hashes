66132405b1a6e7756435ab8eb94d37965639221d
Andrew Poelstra <apoelstra@wpsoftware.net>
2020-12-26 21:45:24 +0000

    move hash_newtype macro so that it's available everywhere 1.29.0 needs it to be

--- src/sha256t.rs
+++ src/sha256t.rs
@@ -141,7 +141,7 @@ macro_rules! sha256t_hash_newtype {
             }
         }
 
-        $crate::hash_newtype!($newtype, $crate::sha256t::Hash<$tag>, 32, $docs, $reverse);
+        hash_newtype!($newtype, $crate::sha256t::Hash<$tag>, 32, $docs, $reverse);
     };
 }
 
